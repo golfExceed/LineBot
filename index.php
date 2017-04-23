c96ff3f1c5fbdfb46673ec4cf3756c2daba61bf7
golfy <golfy@golfys-MacBook-Pro.local>
2017-04-23 17:17:53 +0700

    add index.php

--- index.php
+++ index.php
@@ -22,6 +22,13 @@ if (!is_null($events['events'])) {
 				'text' => $text
 			];
 
+			if($text== "hi"){
+				$messages = [
+				'type' => 'text',
+				'text' => "Fuck"
+				];
+			}
+
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
